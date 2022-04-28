3c0affaa529dfa1b65b3e4ad79c482c6e2e5109c
Caroline <3486507+retronouns@users.noreply.github.com>
2022-04-28 09:33:57 -0600

    made better

--- oop.mjs
+++ oop.mjs
@@ -4,7 +4,7 @@ function log(obj){
 
 //*** Root */
 
-function Animal(sound) {
+var Animal = function(sound) {
     this.sound = sound
     this.speak = () => {
         console.log(this.sound)
@@ -18,38 +18,45 @@ const Pet = function(name, sound){
     this.name = name
 }
 Pet.prototype.__proto__ = Animal.prototype
-Pet.__proto__ = Animal
 
 //*** Second Inheritor */
 
 function Cat(name) {
-    const obj = new Pet(name, 'meow')
-    obj.__proto__ = new.target.prototype
-    return obj
+    return Pet.call(this, name, 'meow')
 }
 Cat.prototype.__proto__ = Pet.prototype
-Cat.prototype.constructor = Cat
 
 //*** Third Inheritor */
 
-class TortoiseShell extends Cat {
-    constructor(name){
-        super(name)
-        this.breed = 'Tortie'
+function TortoiseShell(name) {
+    const obj = new Cat(name)
+    obj.breed = 'Tortie'
+    obj.__proto__ = new.target.prototype
+    return obj
+}
+TortoiseShell.prototype.__proto__ = Cat.prototype
+
+//*** Fourth Inheritor */
+
+class Olive extends TortoiseShell {
+    constructor() {
+        super('Olive')
     }
 }
-
-
 
 log(Animal)
 log(Pet)
 log(Cat)
 log(TortoiseShell)
+log(Olive)
 
-const tortoiseShell = new TortoiseShell("Olive")
-log(tortoiseShell)
-tortoiseShell.speak()
-log(tortoiseShell instanceof TortoiseShell)
-log(tortoiseShell instanceof Cat)
-log(tortoiseShell instanceof Pet)
-log(tortoiseShell instanceof Animal)
+const olive = new Olive
+olive.speak()
+
+log(olive instanceof Olive)
+log(olive instanceof TortoiseShell)
+log(olive instanceof Cat)
+log(olive instanceof Pet)
+log(olive instanceof Animal)
+
+log(olive)
